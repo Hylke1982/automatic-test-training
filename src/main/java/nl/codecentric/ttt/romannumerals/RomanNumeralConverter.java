dcdfaa37c7279879cd06fae370116b43e6195203
Hylke Stapersma <hylke.stapersma@gmail.com>
2016-10-03 19:46:51 +0200

    Roman numeral conversion

---
+++ src/main/java/nl/codecentric/ttt/romannumerals/RomanNumeralConverter.java
@@ -0,0 +1,7 @@
+package nl.codecentric.ttt.romannumerals;
+
+/**
+ * Created by hylke on 03/10/2016.
+ */
+public class RomanNumeralConverter {
+}
---
+++ src/test/java/nl/codecentric/ttt/romannumerals/RomanNumeralConverterTest.java
@@ -0,0 +1,10 @@
+package nl.codecentric.ttt.romannumerals;
+
+/**
+ * Created by hylke on 03/10/2016.
+ */
+public class RomanNumeralConverterTest {
+
+    private RomanNumeralConverter romanNumeralConverter;
+
+}
